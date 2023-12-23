b8542470e5eb9087b9f20ce8d5bcf535bf9c3456
comp-master-byte <demoniac781@gmail.com>
2023-12-23 18:29:01 +0300

    version patch

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ const sayHello = function(name: string): void {
     console.log('hello ' + name + "!")
 }
 
+const dto = {
+    organization: 'runet',
+    hasFrontend: true
+}
+
 module.exports = {
-    sayHello
+    sayHello,
+    dto
 }
