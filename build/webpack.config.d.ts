cc6a5824cb5c4ec7f4c6e4e16f740b43a4219cd3
comp-master-byte <demoniac781@gmail.com>
2023-12-27 12:55:16 +0300

    Правки по сборке

--- build/webpack.config.d.ts
+++
@@ -1,7 +0,0 @@
-import webpack from 'webpack';
-type BuildMode = 'development' | 'production';
-interface EnvVariables {
-    mode: BuildMode;
-}
-declare const _default: (env: EnvVariables) => webpack.Configuration;
-export default _default;
